2e7f6bf2d44d495367cbb993f7f3f3aa43690819
ashrafgate <66882690+ashrafgate@users.noreply.github.com>
2025-05-13 15:15:02 +0530

    Update app.py

--- flask_ecommerce_app_Dynamic/app.py
+++ flask_ecommerce_app_Dynamic/app.py
@@ -59,6 +59,7 @@ def delete_product(product_id):
     connection.close()
 
     return redirect(url_for('home'))
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
